7d13cff25508f6342377423b9120c4a3c942d5ae
Forkiii <osamasalah117@gmail.com>
2024-12-11 12:20:00 +0400

    Icons and Colors added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
-import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return <Stack />;
+import colors from "@/constants/colors";
+import React, { ReactNode } from "react";
+import { StyleSheet, View } from "react-native";
+// Properly type the children prop
+interface RootLayoutProps {
+  children: ReactNode; 
 }
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+ const styles = StyleSheet.create({
+    container: {
+      backgroundColor: colors.background,
+      flex: 1,
+      justifyContent: "center", 
+      alignItems: "center", 
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+export default RootLayout;
---
+++ constants/colors.tsx
@@ -0,0 +1,10 @@
+const colors = {
+    text: "#E3D6F7",
+    background: "#080310",
+    accent: "#D96837",
+    primary: "#5D4090",
+    secondary: "#8B1A31",
+  };
+  
+  export default colors;
+  
