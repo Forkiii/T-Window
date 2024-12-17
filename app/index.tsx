c36e42584e70d3c6cf30863709c01331b0d562aa
Forkiii <84695575+Forkiii@users.noreply.github.com>
2024-12-17 13:07:12 +0400

    extra deleted

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,12 @@
 import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from '../constants/styles';
 export default function Index() {
   return (
    
        
     <View style={styles.container} >
-      <SafeAreaView> 
-      </SafeAreaView>
       <Link href={'../SignUp'}><Text
       style={[styles.headingTwo]}>gotosignin</Text></Link>
     </View>
