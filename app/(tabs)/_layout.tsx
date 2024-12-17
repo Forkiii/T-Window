1a0dc488542ba654da6c871f3fac1d33082df1ac
Mustafa Ahmed <45480939+MidfieId@users.noreply.github.com>
2024-12-17 14:39:40 +0530

    updates

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,52 @@
-import { View, Text, Image } from 'react-native'
-import { Tabs, Redirect, } from 'expo-router'
-import React from 'react'
-import Group from '../../assets/Group'
-
-
-
+import { View, Text, Image } from "react-native";
+import { Tabs, Redirect } from "expo-router";
+import React from "react";
+import Group from "../../assets/Group";
 
 const TabsLayout = () => {
-
-    const tabIcon = ({icon, color, name, focused}) =>{
-        return (
-            <View>
-                <Image src='' />
-            </View>
-        )
-    }
+  const tabIcon = ({ icon, color, name, focused }) => {
+    return (
+      <View>
+        <Image src="" />
+      </View>
+    );
+  };
 
   return (
     <>
-    <Tabs>
+      <Tabs>
         <Tabs.Screen
-         name='home'
-         options={{
-            title:'Home',
-            headerShown:false,
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
             // tabBarIcon:({color, focused}) => (
             // )
-        }}
+          }}
         />
-        
-    </Tabs>
-    </>
-  )
-}
 
-export default TabsLayout
+        <Tabs.Screen
+          name="socials"
+          options={{
+            title: "Home",
+            headerShown: false,
+            // tabBarIcon:({color, focused}) => (
+            // )
+          }}
+        />
+
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Home",
+            headerShown: false,
+            // tabBarIcon:({color, focused}) => (
+            // )
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
